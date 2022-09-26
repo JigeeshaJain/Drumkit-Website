38b9fae47e57cdc10f0ec76d216fe98fa83bdb3a
Jigeesha Jain <90517477+JigeeshaJain@users.noreply.github.com>
2022-09-26 15:55:12 -0400

    Files for building site

---
+++ index.js
@@ -0,0 +1,73 @@
+for (i=0;i<document.querySelectorAll(".drum").length;i++){
+
+
+// Detect button press
+
+document.querySelectorAll("button")[i].addEventListener("click", function () {
+  var buttonInnerHTML=this.innerHTML
+  makesound(buttonInnerHTML)
+  buttonAnimation(buttonInnerHTML)
+  
+});
+
+}
+
+//Detect keyboard press
+
+document.addEventListener("keydown", function(event){
+  makesound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makesound(key){
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    default:console.log();
+      
+  }
+
+}
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
+
+
+
+
+
